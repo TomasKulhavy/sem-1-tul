3299c1dd0cb3915427c52fbfe045f0e2e11bf2f7
Tomáš Kulhavý <43739618+TomasKulhavy@users.noreply.github.com>
2022-01-05 10:19:28 +0100

    SEM1

--- 1215_Kresleni/vektor.m
+++ 1215_Kresleni/vektor.m
@@ -1,16 +1,20 @@
-function Y=vektor(x,y)
-    ro=sqrt(x^2+y^2);
-    fi = atan(x/y);
+function Y=vektor(x)
+    ro=sqrt(x(1)^2+x(2)^2);
+    fi = atan(x(1)/x(2));
     if ro == 0
         fi = 0;
     else
-        fi = atan(x/y);
+        fi = atan(x(1)/x(2));
     end
 
-    if x < 0
+    if x(1) < 0
         fi = fi + pi;
     end
     Y=vektorrofi(ro,fi);
 end
 
-%% zobraz(vektor(1,5))
+%% zobraz(vektor[1;5])
+
+%% číslo podmíněnosti=lambamax/lambdamin %% v abs hodnotě, spočítá jak daleko je od singulární matice
+
+%% zobraz(vektor(F*[1;-1]))
